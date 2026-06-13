c559f0bd347fe8f8db5356e539a9661d8ae2d47c
capygod <clubpeng1920@gmail,com>
2026-06-12 21:14:54 -0500

    cambio de icono a academia en wasapa

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,11 @@ import Header from "../componentes/Header";
 export const metadata: Metadata = {
   title: "Taekwondo Llan-Do",
   description: "¡Conoce más sobre nuestra academia de artes marciales con sede en Cajamarca! :D",
+  openGraph: {
+    title: "Taekwondo Llan-Do",
+    description: "Forjando campeones. Más que un deporte, un estilo de vida.",
+    images: ["/logo/pixel-academia.png"],
+  },
 };
 
 export default function RootLayout({
@@ -15,10 +20,10 @@ export default function RootLayout({
   return (
     <html lang="es" className="scroll-smooth">
       <body className="antialiased">
-        
+
         <Header />
         {children}
-        
+
       </body>
     </html>
   );
